12dcc8f942e9e589c43ef1592e3093495ed0bdb9
Ibrahim Ayoob <ibrahimayoob2131@gmail.com>
2026-08-13 15:55:58 +0530

    Add files via upload

---
+++ Program5/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.toggle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView i1,i2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        i1=findViewById(R.id.BM);
+        i2=findViewById(R.id.Porch);
+        i1.setOnClickListener(this::onClick);
+        i2.setOnClickListener(this::onClick);
+    }
+
+    private void onClick(View view) {
+        if (view.getId()==R.id.BM){
+           i1.setVisibility(View.GONE);
+           i2.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            i2.setVisibility(View.GONE);
+            i1.setVisibility(View.VISIBLE);
+
+        }
+    }
+}
